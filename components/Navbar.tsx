11c10a6eae14c3bc3bd792264017ce85453327d7
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-07-21 16:33:07 +0200

    user auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import MobileNav from '@/components/MobileNav'
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import MobileNav from "@/components/MobileNav";
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-        <Link href="/" className="flex items-center gap-1">
-            <Image src="/icons/logo.svg" width={32} height={32} alt="Yoom logo" className="max-sm:size-10" />
-            <p className="text-[26px] font-extrabold text-white max-sm:hidden"></p>
-        </Link>
-        <div className="flex-between gap-5">
-            {/* User management */}
-            <MobileNav />
-        </div>
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden"></p>
+      </Link>
+      <div className="flex items-center gap-3">
+        <Show when="signed-out">
+          <SignInButton mode="modal">
+            <button className="rounded-md border border-white/20 px-3 py-2 text-sm font-medium text-white transition hover:bg-white/10">
+              Sign in
+            </button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <button className="rounded-md bg-blue-1 px-3 py-2 text-sm font-semibold text-white transition hover:bg-blue-2">
+              Sign up
+            </button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+        <MobileNav />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
